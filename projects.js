446f19902accd0420948379bf2df2c54b724890f
AdamKandias <adamkandias31@gmail.com>
2024-06-01 21:18:31 +0700

    ready to release

---
+++ projects.js
@@ -0,0 +1,199 @@
+// projects.js
+const projects = [
+    {
+        title: "lumajang-tourism",
+        mainTitle: "Lumajang Tourism",
+        mainImage: "img/lumajang-tourism/1.webp",
+        images: [
+            "img/lumajang-tourism/1.webp",
+            "img/lumajang-tourism/2.webp",
+            "img/lumajang-tourism/3.webp",
+            "img/lumajang-tourism/4.webp",
+            "img/lumajang-tourism/5.webp",
+            "img/lumajang-tourism/6.webp",
+        ],
+        category: "Mobile App",
+        techStack: "Dart & Flutter",
+        downloadLink: "https://play.google.com/store/apps/details?id=com.dispar_lumajang.lumajang_tourism",
+        description: `
+            Lumajang Tourism is a mobile application that collaborates with the Ministry of Tourism Lumajang
+            which makes it easier for tourists visiting Lumajang city. This application provides various
+            kinds of accommodation ranging from lodging, vehicles, events, to tour package accommodation, etc.
+            Collaborating with hotels and tourism in the city of Lumajang will of course make it easier for
+            tourists to provide information and find the best place for their holiday in Lumajang.
+        `,
+        features: [
+            "Recommendations for Tourist Attractions",
+            "Lodging Accommodation",
+            "Transport Accommodation",
+            "Event Info",
+            "Tourist Accommodation",
+            "Culinary Accommodations",
+            "Tour Package Accommodation",
+            "Tourist Village",
+            "Shopping Tour",
+            "Money Changer",
+            "Tourist Map",
+            "Lumajang News",
+        ],
+        keywords: "adam kandias, adam kandias project, lumajang tourism, mobile app, tourist attractions, lodging, events, tour packages",
+        shortDescription: "Discover Lumajang with the Lumajang Tourism mobile app. Find accommodations, events, and tour packages.",
+    },
+    {
+        title: "wearshoes.com",
+        mainTitle: "WearShoes.com",
+        mainImage: "img/wearshoes/1.png",
+        images: [
+            "img/wearshoes/1.png",
+            "img/wearshoes/2.png",
+            "img/wearshoes/3.png",
+        ],
+        category: "Mobile App",
+        techStack: "PHP, Bootstrap, Javascript",
+        githubLink: "https://github.com/AdamKandias/wearshoes.com",
+        description: `
+        Catalog application for sales and controlling a company's products, especially shoe products.
+        100% Dynamic and can be controlled by admin.
+        With a dashboard that makes it easier for admins to process product create, read, update, and delete,
+        as well as product reports that can be exported directly to PDF. Made with PHP.
+        `,
+        features: [
+            "Full dynamic dashboard",
+            "Admin dashboard",
+            "User feedback",
+            "Generate PDF report of product stock",
+            "Full dynamic admin control",
+        ],
+    },
+    {
+        title: "takon",
+        mainTitle: "Takon",
+        mainImage: "img/takon/2.png",
+        images: [
+            "img/takon/1.png",
+            "img/takon/2.png",
+            "img/takon/3.png",
+            "img/takon/4.png",
+            "img/takon/5.png",
+        ],
+        category: "Web App",
+        techStack: "PHP, Laravel, Javascript, Bootstrap",
+        githubLink: "https://github.com/AdamKandias/takon",
+        description: `
+        Takon is an educational social media platform designed specifically for students to help them
+        solve their academic problems and build a friendly and supportive community. The platform
+        offers a variety of features that make it easy for students to connect with each other, share
+        resources, and collaborate on assignments. The way it works is similar to Brainly.
+        `,
+        features: [
+            "Q&A: Students can post questions and get answers from other members",
+            "Comments: students can comment on answers given by other students",
+            "Like: Students can like questions, answers, or comments",
+            "Following & Follower: Students can follow other students",
+            "Friends: Students can see updates from their friends",
+            "Role: The more interactions students answer questions, the more students will get interesting roles according to the points they have",
+            "Rank: The more points a student has, the more likely he is to be ranked",
+            "Notification: Users get notifications for every interaction they make, such as if a question has been answered, followed by another user",
+        ],
+        keywords: "adam kandias, adam kandias project, takon, web app",
+        shortDescription: "Takon: An educational social media platform like Brainly, connecting students to solve academic problems, share resources, and collaborate on assignments in a supportive community.",
+    },
+    {
+        title: "restaurant-pos",
+        mainTitle: "Restaurant Point Of Sale (POS)",
+        mainImage: "img/pos/5.jpg",
+        images: [
+            "img/pos/1.jpg",
+            "img/pos/2.jpg",
+            "img/pos/3.jpg",
+            "img/pos/4.jpg",
+            "img/pos/5.jpg",
+            "img/pos/6.jpg",
+        ],
+        category: "Mobile App",
+        techStack: "Dart, Flutter, Firebase",
+        githubLink: "https://github.com/AdamKandias/flutter-restaurant-POS-app",
+        description: `
+        Takon is an educational social media platform designed specifically for students to help them
+        solve their academic problems and build a friendly and supportive community. The platform
+        offers a variety of features that make it easy for students to connect with each other, share
+        resources, and collaborate on assignments. The way it works is similar to Brainly.
+        `,
+        features: [
+            "Order Management",
+            "Status Update (Pending, Paid)",
+            "Table Lock on Order",
+            "Table Release on Paid",
+            "Table Reservation",
+            "Order",
+            "Payment",
+            "Login With Google",
+        ],
+        keywords: "adam kandias, adam kandias project, pos, web app, restaurant, restaurant app, restaurant pos app",
+        shortDescription: "Restaurant POS application to manage and assist restaurant operations, ordering, food, payment and table reservation management.",
+    },
+    {
+        title: "citracker",
+        mainTitle: "Citracker",
+        mainImage: "img/citracker/1.jpg",
+        images: [
+            "img/citracker/1.jpg",
+            "img/citracker/2.jpg",
+            "img/citracker/3.jpg",
+            "img/citracker/4.jpg",
+        ],
+        category: "Mobile App",
+        techStack: "Dart, Flutter, Firebase",
+        githubLink: "https://github.com/AdamKandias/citracker",
+        description: `
+        This is one of the applications I worked on while interning at PT Mascitra. Citracker is an
+        application used to manage projects being worked on in the office. Equipped with various
+        features that make it very easy for users and admins, such as viewing projects, analyzing
+        projects, and printing project reports. It is also equipped with a PDF export feature, so you can
+        easily create documents about projects being worked on in the office.
+        `,
+        keywords: "adam kandias, adam kandias project, citracker, web app, mobile app, tracker app, project management, project management app",
+    },
+    {
+        title: "open-fashion",
+        mainTitle: "Open Fashion",
+        mainImage: "img/open-fashion/2.png",
+        images: [
+            "img/open-fashion/2.png",
+            "img/open-fashion/3.png",
+            "img/open-fashion/4.png",
+            "img/open-fashion/5.png",
+            "img/open-fashion/6.png",
+            "img/open-fashion/7.png",
+        ],
+        category: "Mobile App",
+        techStack: "Dart & Flutter",
+        githubLink: "http://github.com/AdamKandias/fashion-app-design-flutter",
+        description: `
+        Slicing UI Design for fashion app using flutter.
+        By using a state management provider to manage state in the application and to make the application interactive.
+        `,
+    },
+    {
+        title: "e-quran",
+        mainTitle: "E-Qur'an",
+        mainImage: "img/e-quran/1.png",
+        images: [
+            "img/e-quran/1.png",
+            "img/e-quran/2.png",
+            "img/e-quran/3.png",
+        ],
+        category: "Mobile App",
+        techStack: "Dart & Flutter",
+        githubLink: "https://github.com/AdamKandias/e-quran-simple-flutter",
+        description: `
+        E'Quran is a mobile application for reading the Al-Qur'an with a simple, clean, and minimalist
+        appearance, making it easier for users to read more clearly. This application is connected to the
+        Quran API and is also equipped with many features. And this application also has a translation
+        into Indonesian.
+        `,
+        features: [
+            "Indonesian Translation",
+        ],
+    },
+];
